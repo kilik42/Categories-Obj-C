cb367448b2e24934f5dd3154d907a270204929ef
marvin evins <maevins@cps.edu>
2016-06-08 04:10:42 -0500

    Initial Commit

---
+++ Categories/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Categories
+//
+//  Created by marvin evins on 6/8/16.
+//  Copyright © 2016 marvin evins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Categories/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Categories
+//
+//  Created by marvin evins on 6/8/16.
+//  Copyright © 2016 marvin evins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
